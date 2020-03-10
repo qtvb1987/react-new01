881b1ceed7f389d001a321ed4db6cd62aacb31ad
qtvb1987 <qtvb1987@163.com>
2020-03-10 14:04:20 +0800

    react-new测试

---
+++ js/data.js
@@ -0,0 +1,71 @@
+var dataList = {
+    friends: {
+        name: '好友',
+        list: [
+            {
+                username: '九华彩', message: '什么叫做人脉?', vip: true
+            },
+            {
+                username: '柯如意', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '紫言中', message: '为什么会长白头发？', vip: false
+            },
+            {
+                username: '成正思', message: '为什么会长白头发？', vip: false
+            },
+            {
+                username: '长平一', message: '为什么会长白头发？', vip: false
+            },
+            {
+                username: '程平说', message: '为什么会长白头发？', vip: false
+            },
+        ]
+    },
+    families: {
+        name: '家人',
+        list: [
+            {
+                username: '仁一成', message: '什么叫做人脉?', vip: true
+            },
+            {
+                username: '春荷一', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '凭天晨', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '下一', message: '为什么会长白头发？', vip: false
+            },
+            {
+                username: '文平光', message: '为什么会长白头发？', vip: false
+            },
+            {
+                username: '李中上', message: '为什么会长白头发？', vip: false
+            },
+        ]
+    },
+    colleagues: {
+        name: '同事',
+        list: [
+            {
+                username: '刘为', message: '什么叫做人脉?', vip: true
+            },
+            {
+                username: '莫涛', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '彤彤', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '小韩', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: '老刘', message: '为什么会长白头发？', vip: true
+            },
+            {
+                username: 'Reci', message: '为什么会长白头发？', vip: true
+            },
+        ]
+    }
+}
